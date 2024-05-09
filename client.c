06e81d263a01ccc92d72dc39dcf9844673e1e5f8
taciturn023 <newdemotestingbyme@gmail.com>
2024-05-09 22:38:55 +0530

    Add files via upload

--- client.c
+++ client.c
@@ -1,67 +1,58 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-void error(const char *msg){
-    perror(msg);
-    exit(1);
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void createUDPClient(char *ip, int port) {
+    int sockfd;
+    struct sockaddr_in servaddr;
+    int size, i;
+    int *arr;
+    int sum = 0;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    arr = (int *)malloc(size * sizeof(int));
+
+    printf("Enter %d elements of the array:\n", size);
+    for (i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(port);
+    servaddr.sin_addr.s_addr = inet_addr(ip);
+
+    sendto(sockfd, arr, size * sizeof(int), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    printf("Array sent to server.\n");
+
+    int len, n;
+    len = sizeof(servaddr);
+    n = recvfrom(sockfd, &sum, sizeof(sum), MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
+    printf("Sum received from server: %d\n", sum);
+
+    close(sockfd);
+    free(arr);
 }
 
-int main(int argc,char *argv[]){
-
-    int sockfd,portno,n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[255];
-    if(argc<3){
-        fprintf(stderr,"usage: %s hostname portno\n",argv[0]);
-    }
-
-    portno = atoi(argv[2]);
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
-
-    if(sockfd<0){
-        error("socket error");
-    }
-
-    server = gethostbyname(argv[1]);
-    if(server==NULL){
-        fprintf(stderr,"Eroor,no host");
-    }
-
-    bzero((char*) &serv_addr,sizeof(serv_addr));
-
-    serv_addr.sin_family=AF_INET;
-    bcopy((char *) server->h_addr,(char *) &serv_addr.sin_addr.s_addr,server->h_length);
-    serv_addr.sin_port=htons(portno);
-
-    if(connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr))<0){
-        error("connect error");
-    }
-
-
-    while(1){
-        bzero(buffer,255);
-        fgets(buffer,255,stdin);
-        n=write(sockfd,buffer,strlen(buffer));
-        if(n<0)error("write error");
-
-        bzero(buffer,255);
-        n=read(sockfd,buffer,255);
-        if(n<0)error("read error");
-
-        printf("Server: %s",buffer);
-
-        int i=strncmp("Bye",buffer,3);
-        if(i==0)break;
-    }
-close(sockfd);
-return 0;
-
+int main() {
+    char ip[] = "127.0.0.1";
+    int port = PORT;
+    
+    createUDPClient(ip, port);
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,71 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-void error(const char *msg){
-    perror(msg);
-    exit(1);
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void createUDPServer() {
+    int sockfd;
+    struct sockaddr_in servaddr, cliaddr;
+    int len, n;
+    int arr[100];
+    int sum = 0;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    len = sizeof(cliaddr);
+    n = recvfrom(sockfd, (int *)arr, sizeof(arr), MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+    arr[n / sizeof(int)] = '\0';
+
+    for (int i = 0; i < n / sizeof(int); i++) {
+        sum += arr[i];
+    }
+
+    sendto(sockfd, &sum, sizeof(sum), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
+    printf("Sum sent to client.\n");
+
+    close(sockfd);
 }
 
-int main(int argc,char *argv[]){
-
-    if(argc<2){
-        fprintf(stderr,"port number not there");
-        exit(1);
-    }
-
-    int sockfd,newsockfd,portno,n;
-
-    char buffer[255];
-    portno=atoi(argv[1]);
-
-    struct sockaddr_in servAddr,cliAddr;
-
-    socklen_t cliLen;
-
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
-    if(sockfd<0){
-        error("socket error");
-    }
-
-    bzero((char *)&servAddr,sizeof(servAddr));
-
-    servAddr.sin_family=AF_INET;
-    servAddr.sin_addr.s_addr=INADDR_ANY;
-    servAddr.sin_port=htons(portno);
-
-    if(bind(sockfd,(struct sockaddr *)&servAddr,sizeof(servAddr))<0){
-        error("bind error");
-    }
-
-    listen(sockfd,5);
-
-    cliLen=sizeof(cliAddr);
-
-    newsockfd=accept(sockfd,(struct sockaddr *)&cliAddr,&cliLen);
-
-     if(newsockfd<0){error("accept failed");}
-
-    while(1){
-        bzero(buffer,sizeof(buffer));
-        n=read(newsockfd,buffer,sizeof(buffer));
-        if(n<0){error("read error");}
-        printf("client: %s\n",buffer);
-
-        bzero(buffer,sizeof(buffer));
-        fgets(buffer,sizeof(buffer),stdin);
-
-        n=write(newsockfd,buffer,sizeof(buffer));
-        if(n<0){
-            error("write faulded");
-        }
-
-        int i=strncmp("Bye",buffer,3);
-    }
-    close(newsockfd);
-    close(sockfd);
+int main() {
+    createUDPServer();
+    return 0;
 }
